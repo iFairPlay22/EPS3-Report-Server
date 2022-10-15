b87253c663a10ad25379eb7d0994fb111bdcbc41
Ewen Bouquet <ewen.bouquet@gmail.com>
2022-10-15 15:37:37 +0200

    Change file organization + Add view/clear methods

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from datetime import datetime
 import uuid
 import glob
 import os
+import shutil
 import io
 import json
 import torch 
@@ -10,12 +11,12 @@ from PIL import Image
 import ssl
 ssl._create_default_https_context = ssl._create_unverified_context
 
-STORAGE_FOLDER               = "storage"
-STORAGE_INITIAL_IMAGE_FOLDER = os.path.join(STORAGE_FOLDER, "initial-image")
-STORAGE_RESULT_IMAGE_FOLDER  = os.path.join(STORAGE_FOLDER, "result-image")
-STORAGE_RESULT_DATA_FOLDER   = os.path.join(STORAGE_FOLDER, "result-data")
-TEMPLATES_FOLDER             = os.path.join("templates", "html")
-ASSETS_FOLDER                = "assets"
+TEMPLATES_FOLDER                = os.path.join("templates", "html")
+ASSETS_FOLDER                   = "assets"
+STORAGE_FOLDER                  = "storage"
+STORAGE_INITIAL_IMAGE_FILE_NAME = "initial-image"
+STORAGE_RESULT_IMAGE_FILE_NAME  = "result-image"
+STORAGE_RESULT_DATA_FILE_NAME   = "result-data"
 
 def createFolderIfNotExists(path):
 	if not os.path.exists(path):
@@ -27,50 +28,42 @@ def createFoldersIfNotExists(paths):
 
 createFoldersIfNotExists([
 	STORAGE_FOLDER,
-	STORAGE_INITIAL_IMAGE_FOLDER, 
-	STORAGE_RESULT_IMAGE_FOLDER,
-	STORAGE_RESULT_DATA_FOLDER,
 	TEMPLATES_FOLDER, 
 	ASSETS_FOLDER
 ])
 
 app = Flask(__name__, template_folder=TEMPLATES_FOLDER)
 
+def sanitizeFileName(string: str):
+	return "".join(char for char in str(string) if char.isalnum())
+
 # Upload the report files in the STORAGE_FOLDER
 # Paths:
 # 	> "STORAGE_INITIAL_IMAGE_FOLDER/YEARy-MONTHm-DAYd/YEARy-MONTHm-DAYd-HOURh-MINUTEm-SECONDs-ID.EXTENSION"	
 # 	> "STORAGE_RESULT_IMAGE_FOLDER/YEARy-MONTHm-DAYd/YEARy-MONTHm-DAYd-HOURh-MINUTEm-SECONDs-ID.EXTENSION"
 # 	> "STORAGE_RESULT_DATA_FOLDER/YEARy-MONTHm-DAYd/YEARy-MONTHm-DAYd-HOURh-MINUTEm-SECONDs-ID.json"
-def uploadReportFiles(building_id, initial_file):
+def uploadReportFiles(building_id : str, initial_file):
 
-	building_id = str(building_id)
-
-	# Time
-	dt  = datetime.now()
-	d   = dt.strftime('%dd-%mm-%Yy')
-	t   = dt.strftime('%Hh-%Mm-%Ss')
+	date_time   = datetime.now()
+	date        = sanitizeFileName(date_time.strftime('%Y-%m-%d'))
+	time        = sanitizeFileName(date_time.strftime('%H-%M-%S'))
+	building_id = sanitizeFileName(building_id)
+	analysis_id = sanitizeFileName(uuid.uuid4())
 
 	# File structure
 	extension = initial_file.filename.split(".")[-1]
-	file_name = building_id + "-" + d + "-" + t
 
-	initial_image_building_folder_path = os.path.join(STORAGE_INITIAL_IMAGE_FOLDER, building_id) 
-	result_image_building_folder_path  = os.path.join(STORAGE_RESULT_IMAGE_FOLDER,  building_id) 
-	result_data_building_folder_path   = os.path.join(STORAGE_RESULT_DATA_FOLDER,   building_id) 
-	initial_image_time_folder_path     = os.path.join(initial_image_building_folder_path, d) 
-	result_image_time_folder_path      = os.path.join(result_image_building_folder_path,  d) 
-	result_data_time_folder_path       = os.path.join(result_data_building_folder_path,   d) 
-	initial_image_full_path   		   = os.path.join(initial_image_time_folder_path, file_name + "." + extension) 
-	result_image_full_path    		   = os.path.join(result_image_time_folder_path,  file_name + "." + extension) 
-	result_data_full_path     		   = os.path.join(result_data_time_folder_path,   file_name + ".json") 
+	initial_image_time_analysis_folder_path     = os.path.join(STORAGE_FOLDER, building_id, date, analysis_id) 
+	result_image_time_analysis_folder_path      = os.path.join(STORAGE_FOLDER, building_id, date, analysis_id) 
+	result_data_time_analysis_folder_path       = os.path.join(STORAGE_FOLDER, building_id, date, analysis_id) 
+	initial_image_full_path   		   = os.path.join(initial_image_time_analysis_folder_path, STORAGE_INITIAL_IMAGE_FILE_NAME + "." + extension) 
+	result_image_full_path    		   = os.path.join(result_image_time_analysis_folder_path,  STORAGE_RESULT_IMAGE_FILE_NAME  + "." + extension) 
+	result_data_full_path     		   = os.path.join(result_data_time_analysis_folder_path,   STORAGE_RESULT_DATA_FILE_NAME   + "." + "json") 
 
 	createFoldersIfNotExists([
-		initial_image_building_folder_path,
-		result_image_building_folder_path, 
-		result_data_building_folder_path,
-		initial_image_time_folder_path,
-		result_image_time_folder_path, 
-		result_data_time_folder_path
+		initial_image_time_analysis_folder_path,
+		result_image_time_analysis_folder_path, 
+		result_data_time_analysis_folder_path
 	])
 
 	# Save original file
@@ -85,8 +78,10 @@ def uploadReportFiles(building_id, initial_file):
 	predictions_nb  = results_data.shape[0]
 	predictions_json = {
 		"metadata": {
-			"id": id,
-			"time": dt.strftime('%d/%m/%Y %H:%M:%S'),
+			"building_id": str(building_id),
+			"day":         str(date),
+			"time":        str(time),
+			"analysis_id": str(analysis_id)
 		},
 		"predictions": [
 			{
@@ -107,7 +102,7 @@ def uploadReportFiles(building_id, initial_file):
 	}
 	
 	with open(result_data_full_path, 'w') as f:
-		f.write(json.dumps(predictions_json))
+		f.write(json.dumps(predictions_json, indent=4))
 
 	# Image
 	result_img   = results.render()[0]
@@ -115,10 +110,32 @@ def uploadReportFiles(building_id, initial_file):
 	result_file.save(result_image_full_path)
 
 	# Return full paths
-	return predictions_json
+	return getAnalysis(building_id, date, analysis_id)
+
+def getAnalysis(building_id, day_string, analysis_id):
+    	
+	analysis_folder_full_path = os.path.join(STORAGE_FOLDER, sanitizeFileName(building_id), sanitizeFileName(day_string), sanitizeFileName(analysis_id))    	
+	
+	# Result data
+	result_data_full_path = os.path.join(analysis_folder_full_path, STORAGE_RESULT_DATA_FILE_NAME + ".json")
+	with open(result_data_full_path, "r") as f:
+		analysys_data = json.load(f)
+
+	# Images
+	for analysis_file_path in os.listdir(analysis_folder_full_path):
+		analysis_file_full_path = os.path.join(analysis_folder_full_path, analysis_file_path)
+		if os.path.isfile(analysis_file_full_path):
+			
+			if analysis_file_path.startswith(STORAGE_INITIAL_IMAGE_FILE_NAME):
+				analysys_data[STORAGE_INITIAL_IMAGE_FILE_NAME] = analysis_file_full_path
+					
+			if analysis_file_path.startswith(STORAGE_RESULT_IMAGE_FILE_NAME):
+				analysys_data[STORAGE_RESULT_IMAGE_FILE_NAME]  = analysis_file_full_path
+
+	return analysys_data
 
 @app.route('/api/analyse/<building_id>')
-def apiAnalysisDetection(building_id):
+def apiAnalyse(building_id : str):
 
 	# Requests
 	files = list(request.files.items())
@@ -133,11 +150,30 @@ def apiAnalysisDetection(building_id):
 
 	return results
 
-@app.route('/api/get/<building_id>/<day_string>')
-def apiAnalysisDetection(building_id, day_string):
+@app.route('/api/view/<building_id>/<day_string>')
+def apiView(building_id : str, day_string : str):
 
-	for f in glob.glob(f"")
+	analysis_results = []
 
+	# Foreach analysis
+	main_directory = os.path.join(STORAGE_FOLDER, sanitizeFileName(building_id), sanitizeFileName(day_string))
+	for analysis_folder_path in os.listdir(main_directory):
+		analysis_folder_full_path = os.path.join(main_directory, analysis_folder_path)
+		if os.path.isdir(analysis_folder_full_path):
+			
+			# One analysis
+			analysis_id = analysis_folder_path
+			analysys_data = getAnalysis(building_id, day_string, analysis_id)
+			analysis_results.append(analysys_data)
+						
+	return analysis_results
+
+@app.route('/api/clear')
+def apiClear():
+    shutil.rmtree(STORAGE_FOLDER)
+    createFolderIfNotExists(STORAGE_FOLDER)
+
+    return "Done"
 
 @app.route('/')
 def home():
@@ -145,5 +181,5 @@ def home():
 
 if __name__ == '__main__':
 	
-	model = torch.hub.load('ultralytics/yolov5', 'custom', path=os.path.join(ASSETS_FOLDER, "weights/best.pt"), force_reload=True)
+	model = torch.hub.load('ultralytics/yolov5', 'custom', path=os.path.join(ASSETS_FOLDER, "weights/best.pt")) # , force_reload=True
 	app.run(debug=True)
